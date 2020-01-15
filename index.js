81041515664c6b7f3575ad36d859854992427785
Prathamesh <prathu48@gmail.com>
2020-01-15 15:31:09 +0530

    theme created

---
+++ index.js
@@ -0,0 +1,26 @@
+exports.decorateConfig = config =>
+  Object.assign({}, config, {
+    borderColor: config.borderColor || 'transparent',
+    cursorColor: config.cursorColor || 'cyan',
+    backgroundColor: 'blue',
+    foregroundColor: 'lightWhite',
+    selectionColor: '#69696980',
+    color: {
+      black: '#212121',
+      red: '#FF4646',
+      green: '#02D07D',
+      yellow: '#FDE24F',
+      blue: '#00214D',
+      magenta: '#FF5470',
+      cyan: '#00ECD2',
+      white: '#ffff',
+      lightBlack: '#3D3D3D',
+      lightRed: '#FF5F5F',
+      lightGreen: '#35FDAD',
+      lightYellow: '#FEEA81',
+      lightBlue: '#185FBD',
+      lightMagenta: '#FF798F',
+      lightCyan: '#6CFFEF',
+      lightWhite: '#fffffe',
+    },
+  });
